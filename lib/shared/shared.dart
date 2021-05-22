af6ebe5f91ac7e172c6562e301575923a0bd1764
Nailul Firdaus <nail.nafir@gmail.com>
2021-05-22 18:13:13 +0700

    add theme custom settings

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,1 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 part 'theme.dart';
--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,1 +1,9 @@
 part of 'shared.dart';
+
+const double defaultMargin = 24;
+
+Color bgLight = Color(0xFFF5F5F5);
+Color bgDark = Color(0xFF15153F);
+
+TextStyle blackTextFont = GoogleFonts.poppins().copyWith(color: Colors.black);
+TextStyle whiteTextFont = GoogleFonts.poppins().copyWith(color: Colors.white);
