55ed9d086a9056291b7cf47c32b67fb314444d67
Nailul Firdaus <nail.nafir@gmail.com>
2021-05-28 07:51:18 +0700

    change home page ui

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -3,6 +3,15 @@ part of 'pages.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: bgLight,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Text("Home Page"),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ngelancong/shared/shared.dart';
 
 part 'home_page.dart';
