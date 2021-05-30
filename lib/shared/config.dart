9e5d52bc29206554730beee182692f9539667d0d
Nailul Firdaus <nail.nafir@gmail.com>
2021-05-30 11:28:32 +0700

    add get device size to config

---
+++ lib/shared/config.dart
@@ -0,0 +1,8 @@
+part of 'shared.dart';
+
+const double defaultMargin = 24;
+
+double getDeviceWidth(BuildContext context) =>
+    MediaQuery.of(context).size.width;
+double getDeviceHeight(BuildContext context) =>
+    MediaQuery.of(context).size.height;
--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -3,3 +3,4 @@ import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 part 'theme.dart';
+part 'config.dart';
