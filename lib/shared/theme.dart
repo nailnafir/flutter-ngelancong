baacfd9eaca76105e5ff47fdb01bd706a35c82e2
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-01 10:46:48 +0700

    add gradient color theme

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -5,3 +5,16 @@ Color bgDark = Color(0xFF15153F);
 
 TextStyle blackTextFont = GoogleFonts.poppins().copyWith(color: Colors.black);
 TextStyle whiteTextFont = GoogleFonts.poppins().copyWith(color: Colors.white);
+
+class MyTheme {
+  static List<Color> gradientColorTheme() => [
+        Color(0xFF42E695),
+        Color(0xFF3BB2B8),
+      ];
+
+  static Gradient gradientTheme() => LinearGradient(
+        colors: MyTheme.gradientColorTheme(),
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      );
+}
