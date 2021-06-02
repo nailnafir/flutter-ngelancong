b2c386d2b21f6b0467d2794bc5a8722086efd72b
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-02 15:53:20 +0700

    changed config add new class

--- lib/shared/config.dart
+++ lib/shared/config.dart
@@ -1,8 +1,10 @@
 part of 'shared.dart';
 
-const double defaultMargin = 24;
+class MyConfig {
+  static const double defaultMargin = 24;
 
-double getDeviceWidth(BuildContext context) =>
-    MediaQuery.of(context).size.width;
-double getDeviceHeight(BuildContext context) =>
-    MediaQuery.of(context).size.height;
+  static double getDeviceWidth(BuildContext context) =>
+      MediaQuery.of(context).size.width;
+  static double getDeviceHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height;
+}
