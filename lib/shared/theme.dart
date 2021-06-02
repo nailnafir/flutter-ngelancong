304d33420f3ce4fb85e03b019eda1600cddd71cb
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-02 15:48:33 +0700

    changed theme add new class

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,11 +1,5 @@
 part of 'shared.dart';
 
-Color bgLight = Color(0xFFF5F5F5);
-Color bgDark = Color(0xFF15153F);
-
-TextStyle blackTextFont = GoogleFonts.poppins().copyWith(color: Colors.black);
-TextStyle whiteTextFont = GoogleFonts.poppins().copyWith(color: Colors.white);
-
 class MyTheme {
   static List<Color> gradientColorTheme() => [
         Color(0xFF42E695),
@@ -17,4 +11,12 @@ class MyTheme {
         begin: Alignment.topCenter,
         end: Alignment.bottomCenter,
       );
+
+  static Color bgLight = Color(0xFFF5F5F5);
+  static Color bgDark = Color(0xFF15153F);
+
+  static TextStyle blackTextFont =
+      GoogleFonts.poppins().copyWith(color: Colors.black);
+  static TextStyle whiteTextFont =
+      GoogleFonts.poppins().copyWith(color: Colors.white);
 }
